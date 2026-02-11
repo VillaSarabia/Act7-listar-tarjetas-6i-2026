587f9f16df467f05b80ffe5e9ec30c68d2623ea8
a.23308051280227 <a.23308051280227@cbtis128.edu.mx>
2026-02-11 20:33:22 +0000

    otra vez

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,120 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(AppDecktcg());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AppDecktcg extends StatelessWidget {
+  const AppDecktcg({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Productos());
+  }
+} //FIN CLASE APPDECKTCG
+
+class Productos extends StatelessWidget {
+  const Productos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        title: const Text(
+          'Tienda de coleccionables DECK TCG',
+          style: TextStyle(
+            color: Colors.white, // Letra blanca
+            fontWeight: FontWeight.bold,
+          ),
         ),
+        backgroundColor: const Color(
+          0xFFD32F2F,
+        ), // Un rojo "moderno" (Material Red 700)
+        elevation: 4, // Sombra ligera para dar profundidad
+        leading: const Icon(
+          Icons.ac_unit, // Icono que asemeja un juguete/figura
+          color: Colors.white,
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.style,
+              color: Colors.white,
+            ), // Icono de tarjetas
+            onPressed: () {
+              // Acción al presionar
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.person,
+              color: Colors.white,
+            ), // Icono de usuario
+            onPressed: () {
+              // Acción al presionar
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.settings,
+              color: Colors.white,
+            ), // Icono de ajustes
+            onPressed: () {
+              // Acción al presionar
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [
+          _crearTarjeta(
+            'Mazo Yahel Villa Sarabia',
+            'Colección Seto Kaiba',
+            'https://raw.githubusercontent.com/VillaSarabia/imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/pkmcarta%201.png', // URL directa
+          ),
+          _crearTarjeta(
+            'Mazo Entrenador Pokemon',
+            'Colección Yugi Muto',
+            'https://raw.githubusercontent.com/VillaSarabia/imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/pkmncarta%202.jpg', // URL directa
+          ),
+          _crearTarjeta(
+            'Héroe Elemental',
+            'Colección Jaden Yuki',
+            'https://raw.githubusercontent.com/VillaSarabia/imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/pkmncarta%203.jpg', // URL directa
+          ),
+          _crearTarjeta(
+            'Stardust Dragon',
+            'Colección Yusei Fudo',
+            'https://raw.githubusercontent.com/VillaSarabia/imagenes-para-Flutter-6to-I-Fecha-11-Feb-2026/refs/heads/main/pkmcarta%204.png', // URL directa
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Widget personalizado con el parámetro urlImagen añadido
+  Widget _crearTarjeta(String titulo, String subtitulo, String urlImagen) {
+    return Card(
+      color: Colors.white,
+      elevation: 5,
+      margin: const EdgeInsets.only(bottom: 16.0),
+      child: ListTile(
+        leading: CircleAvatar(
+          // Ahora usamos el parámetro urlImagen directamente aquí
+          backgroundImage: NetworkImage(urlImagen),
+        ),
+        title: Text(
+          titulo,
+          style: const TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        subtitle: Text(
+          subtitulo,
+          style: const TextStyle(color: Colors.black54),
+        ),
+        trailing: const Icon(Icons.thumb_up, color: Colors.blue),
+      ),
     );
   }
 }
